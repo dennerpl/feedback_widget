3fdbe035f45cb17f1336038969be9961cf6d5179
dennerpl <denner.souzapl@gmail.com>
2022-05-16 00:02:39 -0300

    guardando projeto

---
+++ server/src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,22 @@
+import { MailAdapter, SendMailData } from "../mail-adapter";
+import nodemailer from 'nodemailer';
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "8f23c0cc59accc",
+    pass: "4042e2941f2c7d"
+  }
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+  async sendMail({ subject, body }: SendMailData) {
+    await transport.sendMail({
+      from: 'Equipe Feedget <oi@feedget.com>',
+      to: 'Denner Carneiro <denner.souzapl@gmail.com>',
+      subject,
+      html: body,
+    })
+  }
+}
---
+++ server/src/repositories/prisma/prisma-feedbacks-repository.ts
@@ -0,0 +1,13 @@
+import { prisma } from "../../prisma"
+import { FeedbacksRepository, FeedbackCreateData } from "../feedbacks-repository"
+export class PrismaFeedbacksRepository implements PrismaFeedbacksRepository {
+  async create({ type, comment, screenshot}: FeedbackCreateData) {
+    await prisma.feedback.create({
+      data: {
+        type,
+        comment,
+        screenshot
+      }
+    })
+  }
+}
